8cf0ba697cf6c2e512653849ab0608b3ec8a43bb
Arjun Dixit <technoarjun@gmail.com>
2023-10-26 22:04:44 -0700

    Update NativeEngine.swift

--- Sources/Engine/NativeEngine.swift
+++ Sources/Engine/NativeEngine.swift
@@ -34,7 +34,7 @@ public class NativeEngine: NSObject, Engine, URLSessionDataDelegate, URLSessionW
     public func start(request: URLRequest) {
         let session = URLSession(configuration: URLSessionConfiguration.default, delegate: self, delegateQueue: nil)
         task = session.webSocketTask(with: request)
-        task.maximumMessageSize = 16777216
+        task?.maximumMessageSize = 16777216
         doRead()
         task?.resume()
     }
